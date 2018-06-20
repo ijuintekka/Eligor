bd218394e61a83283b2513c909ffb6f8273705cf
ijuintekka <ijuintekka@hotmail.com>
2018-06-20 18:58:13 +0100

    Progress bar form icon.

--- Eligor/Progress.Designer.cs
+++ Eligor/Progress.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Progress));
             this.Cancel = new System.Windows.Forms.Button();
             this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
             this.ProgressBar2 = new System.Windows.Forms.ProgressBar();
@@ -36,7 +37,7 @@
             // Cancel
             // 
             this.Cancel.Location = new System.Drawing.Point(77, 44);
-            this.Cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(2);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(56, 25);
             this.Cancel.TabIndex = 10;
@@ -55,7 +56,7 @@
             // ProgressBar2
             // 
             this.ProgressBar2.Location = new System.Drawing.Point(12, 21);
-            this.ProgressBar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ProgressBar2.Margin = new System.Windows.Forms.Padding(2);
             this.ProgressBar2.Name = "ProgressBar2";
             this.ProgressBar2.Size = new System.Drawing.Size(184, 19);
             this.ProgressBar2.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
@@ -70,7 +71,8 @@
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.ProgressBar1);
             this.Controls.Add(this.ProgressBar2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(222, 114);
             this.MinimumSize = new System.Drawing.Size(222, 114);
             this.Name = "Progress";
